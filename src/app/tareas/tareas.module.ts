13e4718d2b3ac957cdbbc850d85b53db99021dcb
Sergio_Nahin <sergiontr280@gmail.com>
2024-06-03 19:38:19 -0600

    crear componente grid

--- src/app/tareas/tareas.module.ts
+++ src/app/tareas/tareas.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SubtituloComponent } from './subtitulo/subtitulo.component';
 import { ListadoTareasComponent } from './listado-tareas/listado-tareas.component';
+import { GridComponent } from './grid/grid.component';
 
 
 
 @NgModule({
   declarations: [
     SubtituloComponent,
-    ListadoTareasComponent
+    ListadoTareasComponent,
+    GridComponent
   ],
   imports: [
     CommonModule
